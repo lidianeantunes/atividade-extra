f991bc8377b3a424f89bb6a8b208d769335e2a9a
lidianeantunes <lidianeantunes346@gmail.com>
2019-07-19 17:47:04 -0300

    atividade extra

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-let idade = Number(prompt("digite sua idade"));
-let aposentado =(prompt("vc é aposentado? (sim/nao)"));
-let estudante = (prompt("vc é estudante?"));
+// let idade = Number(prompt("digite sua idade"));
+// let aposentado =(prompt("vc é aposentado? (sim/nao)"));
+// let estudante = (prompt("vc é estudante?"));
 
-aposentado.toLocaleUpperCase();
+// aposentado = aposentado.toUpperCase();
 
-if (idade<=12){
-    console.log ("entrada 15 reais");
-} else if (idade >=13 && idade <= 17){
-    console.log ("entrada 25 reais");
-} else if (idade >= 18 && idade<=59){
-    console .log ("entrada 35 reais");
-} else if (idade >= 60 || aposentado === "SIM");{
-      console .log("entrada gratuita");  
-} 
+// if (idade<=12){
+//     console.log ("entrada 15 reais");
+// } else if (idade >=13 && idade <= 17){
+//     console.log ("entrada 25 reais");
+// } else if (idade >= 18 && idade<=59){
+//     console .log ("entrada 35 reais");
+// } else if (idade >= 60 || aposentado === "SIM");{
+//       console .log("entrada gratuita");  
+// } 
+
+
+let data = Number(prompt("digite sua data indesejada"));
+let semana = Number(prompt("digite sua semana indesejada"));
+ 
+if (data =1){
+    console.log ("dia da semana domingo");
+}
+else if (data =2){
+    console.log ("dia da semana segunda-feira");
+}
+else if (data=3){
+    console.log ("dia da semana terça-feira");
+}
+else if (data=4){
+    console.log ("dia da seman quarta-feira");
+}
+else if (data=5){
+    console.log("dia da semana quinta-feira");
+}
+else if (data=6){
+    console.log ("dia da semana sexta-feira");
+}
+else if (data=7){
+    console.log ("dia da semana sabado");
+}
